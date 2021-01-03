bce05c6d9da6ff03c2d79e2e50eeba7448b40daa
chan18 <chan18@users.noreply.github.com>
2021-01-03 11:24:46 +0530

    added statiq

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+using Statiq.App;
+using Statiq.Common;
+using Statiq.Web;
+
+namespace MySite
+{
+  public class Program
+  {
+   public static async Task<int> Main(string[] args) =>
+      await Bootstrapper
+        .Factory
+        .CreateWeb(args)
+        .RunAsync();
+  }
+}
